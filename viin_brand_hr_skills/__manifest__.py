275705443ede762f4079dd750bcfc95b42920cd9
duyquyen96 <duyquyencnt55@gmail.com>
2023-06-15 13:49:23 +0700

    [IMP] viin_brand_hr*: replace odoo brand by viindoo

--- viin_brand_hr_skills/__manifest__.py
+++ viin_brand_hr_skills/__manifest__.py
@@ -41,6 +41,7 @@ Module này sẽ thay đổi giao diện module Skills Management theo thương 
 
     # always loaded
     'data': [
+        'demo/hr_resume_demo.xml',
         'views/hr_views.xml',
     ],
     'installable': True,
